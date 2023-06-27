103ba441f65f7d90cc7a6ffb604617522454a58e
Ruslan Gorenkov <ruslangorenkov@gmail.com>
2023-06-28 00:16:37 +0300

    Add gitignore

--- backend/shmeks/shmeks/settings.py
+++ backend/shmeks/shmeks/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^=1-p%6+=g*ko-+($o1wjf(!!^m$c#l69-$rc-=_jf^cc*wraj'
+# SECRET_KEY = 'django-insecure-^=1-p%6+=g*ko-+($o1wjf(!!^m$c#l69-$rc-=_jf^cc*wraj'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-^=1-p%6+=g*ko-+($o1wjf(!!^m$c#l69-$rc-=_jf^cc*wraj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
